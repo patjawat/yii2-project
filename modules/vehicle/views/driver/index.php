ce7e7322b6b3bacc79f4cef15fb35fe83bfc7d50
patjawat <patjawat@gmail.com>
2022-11-23 11:19:50 +0700

    update

--- modules/vehicle/views/driver/index.php
+++ modules/vehicle/views/driver/index.php
@@ -38,9 +38,9 @@ $this->registerCss($this->render('style.css'));
                 
                 <?=Html::a('<i class="fa-solid fa-calendar-days"></i> ปฎิทิน', ['/vehicle/driver/job','id' => $model['id']],['class' => ' btn btn-primary a-modal'])?>
                     <!-- <span class="btn btn-primary view_calendar"><i class="fa-solid fa-calendar-days"></i> ปฎิทิน</span> -->
-                    <?php $link = isset(Json::decode($model['data_json'])['link']) ? Json::decode($model['data_json'])['link'] : '' ?>
-                    <?php if (isset($link)):?>
-                        <?=Html::a('<i class="fa-regular fa-star"></i> แบบประเมิน',Json::decode($model['data_json'])['link'],['class' =>'btn btn-outline-warning','target' => '_blank'])?>
+                    <?php $link = Json::decode($model['data_json']); ?>
+                    <?php if (isset($link) && $link['link']):?>
+                        <?=Html::a('<i class="fa-regular fa-star"></i> แบบประเมิน',[$link['link']],['class' =>'btn btn-outline-warning','target' => '_blank'])?>
                         <?php else:?>
                     <a href="#" class="btn btn-outline-secondary"><i class="fa-regular fa-star"></i> แบบประเมิน</a>
                     <?php endif;?>
