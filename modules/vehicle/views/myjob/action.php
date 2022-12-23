3be57550a383a9a63e4cb13bc9e22a34b3d9e58e
patjawat <patjawat@gmail.com>
2022-12-23 11:09:24 +0700

    a

--- modules/vehicle/views/myjob/action.php
+++
@@ -1,9 +0,0 @@
-<?php
-use yii\helpers\Html;
-?>
-<div>
-    <?php echo Html::a('<i class="fa-regular fa-pen-to-square"></i> แก้ไข', ['update', 'id' => $model->id], [
-            'class' => 'btn btn-sm btn-warning',
-        ]) ?>
-       
-</div>
