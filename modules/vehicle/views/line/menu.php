d057fb6ce9e82f3a772c26c38fada2c8bf60b701
patjawat <patjawat@gmail.com>
2023-01-31 13:42:44 +0700

    ี

--- modules/vehicle/views/line/menu.php
+++ modules/vehicle/views/line/menu.php
@@ -10,9 +10,9 @@ $this->title = 'เมนูอื่นๆ';
         <div class="card text-center border-0 shadow-md mb-3 bg-body-tertiary rounded">
 
             <div class="card-body">
-                <h5 class="card-title">QR-Code</h5>
+                <h5 class="card-title">จองรถ</h5>
                 <p class="card-text">Line OA </p>
-                <?=Html::a('<i class="fa-regular fa-hand-pointer"></i> เพิ่มเติม...',['/vehicle/line/qrcode'],['class' => 'btn btn-primary']);?>
+                <?=Html::a('<i class="fa-regular fa-hand-pointer"></i> เพิ่มเติม...',['/vehicle/line'],['class' => 'btn btn-primary']);?>
             </div>
         </div>
     </div>
